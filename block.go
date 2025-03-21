99cd1ff63bdb8d02a74a407317c33e6668251801
KikiRiki004 <krivonosovk.dev@gmail.com>
2025-03-21 23:43:13 +0100

    Proof-of-work

--- block.go
+++ block.go
@@ -1,33 +1,31 @@
 package main
 
 import (
-	"bytes"
-	"crypto/sha256"
-	"strconv"
 	"time"
 )
 
 // Block keeps block headers
 type Block struct {
-	Timestamp     int64  // Time when block was created
-	Data          []byte // Valuable information
+	Timestamp     int64
+	Data          []byte
 	PrevBlockHash []byte
 	Hash          []byte
-}
-
-// SetHash calculates and sets block hash
-func (b *Block) SetHash() {
-	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))                       // Convert timestamp to string and to byte slice
-	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{}) // Concatinate byte slices
-	hash := sha256.Sum256(headers)                                                // Computes hash, returns array of 32 bytes
-
-	b.Hash = hash[:] // Converts hash into slice and assigns it to the b.Hash field in the block
+	Nonce         int
 }
 
 // NewBlock creates and returns Block
-// Golang convention to use constructor functions New<Type> to create and return a pointer to a new instance of a struct
 func NewBlock(data string, prevBlockHash []byte) *Block {
-	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}}
-	block.SetHash()
+	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}, 0}
+	pow := NewProofOfWork(block)
+	nonce, hash := pow.Run()
+
+	block.Hash = hash[:]
+	block.Nonce = nonce
+
 	return block
+}
+
+// NewGenesisBlock creates and returns genesis Block
+func NewGenesisBlock() *Block {
+	return NewBlock("Genesis Block", []byte{})
 }
--- blockchain.go
+++ blockchain.go
@@ -2,7 +2,7 @@ package main
 
 // Blockchain keeps a sequence of Blocks
 type Blockchain struct {
-	blocks []*Block // A slice of pointers to Block structs
+	blocks []*Block
 }
 
 // AddBlock saves provided data as a block in the blockchain
@@ -10,11 +10,6 @@ func (bc *Blockchain) AddBlock(data string) {
 	prevBlock := bc.blocks[len(bc.blocks)-1]
 	newBlock := NewBlock(data, prevBlock.Hash)
 	bc.blocks = append(bc.blocks, newBlock)
-}
-
-// Genesis block is the firsst block in the blockchain
-func NewGenesisBlock() *Block {
-	return NewBlock("Genesis Block", []byte{}) // Passes empty PrevBlockHash []byte{}, since its the first block
 }
 
 // NewBlockchain creates a new Blockchain with genesis Block
